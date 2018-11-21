0cca9a5fcad5ad041c49df4aed620507c8094a7e
ziyi huang <hzy86@uw.edu>
2018-11-20 16:17:18 -0800

    newer version

--- grading.py
+++ grading.py
@@ -1,16 +1,17 @@
+"""
+A program to automate unzipping and Canvas submission files.
+
+@author: Ziyi Huang
+
+date: 11/20/18
+"""
+
 from zipfile import ZipFile
 import os
 import sys
 
 dir = os.listdir('.')
 
-# for grading specific file
-# with ZipFile('mangalamsaket_late_3722525_51186346.zip', 'r') as zip:
-#     # zip.extract('hw1/main.py')
-#     print zip.listdir()
-#     print 'Extracted main.py from', file
-
-# first loop, done 13
 for file in dir:
     try:
         if file.endswith('hw3.zip') or file.endswith('HW3.zip'):
@@ -24,18 +25,3 @@ for file in dir:
             print "skipping file not end with hw3.zip", "file name:", file
     except:
         raise
-    # except:
-    #     print "errors with ", file
-    #     continue
-
-# working with other files with error reason
-# file = dir[int(sys.argv[1])]
-# if file.endswith('hw1.zip'):
-#     print 'Working with', file
-#     filename = 'hw1/main.py'
-#     with ZipFile(file, 'r') as zip:
-#         zip.extract(filename)
-#     execfile('hw1/main.py')
-#
-# else:
-#     print file, "skipping file not end with hw1.zip"
